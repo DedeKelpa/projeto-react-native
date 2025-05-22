3abe83baac40b4ea29b696269d23b906b103018e
David Gabriel <146039653+DedeKelpa@users.noreply.github.com>
2025-05-21 22:55:53 -0300

    Update firebase.js
    Outro firebase (firebase antigo estava dando problema)

--- config/firebase.js
+++ config/firebase.js
@@ -1,19 +1,20 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvyB09sOBKEH6-0EivW9xW5hVtK0ZHSXg",
-  authDomain: "projeto--gerenciamento-habitos.firebaseapp.com",
-  databaseURL: "https://projeto--gerenciamento-habitos-default-rtdb.firebaseio.com",
-  projectId: "projeto--gerenciamento-habitos",
-  storageBucket: "projeto--gerenciamento-habitos.firebasestorage.app",
-  messagingSenderId: "362207572860",
-  appId: "1:362207572860:web:caf07e40bd9cca71e2eaf9"
+  apiKey: "AIzaSyDsV55IatKZfedWPwZcgtAVmXKV7gVhWjg",
+  authDomain: "habitos-app-8702b.firebaseapp.com",
+  databaseURL: "https://habitos-app-8702b-default-rtdb.firebaseio.com",
+  projectId: "habitos-app-8702b",
+  storageBucket: "habitos-app-8702b.firebasestorage.app",
+  messagingSenderId: "266771518187",
+  appId: "1:266771518187:web:246eba1cafc0abe2dd64ae",
+  measurementId: "G-Y3MPDQRX8X"
 };
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-export const auth = firebase.auth();
+
 export const database = firebase.database();
 export default firebase;
